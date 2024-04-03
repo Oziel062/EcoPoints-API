bf40a04683eb7e0de3d569bcfbd3044dcf9c1919
Oziel Sousa <138177458+Oziel062@users.noreply.github.com>
2024-04-03 14:29:06 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
 import express from "express";
 import { testConnection } from "./database/connection.js";
 import { routers } from "./routes/index.routes.js";
-import cors from "cors";
+
 
 
 export const app = express()
 const port = 3000
 app.use(express.json())
-app.use(cors({
-    origin: "*"
-}))
+
 app.use(routers)
 
 app.listen(port, () => {
